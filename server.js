ab1597ae4f0a9f6a5a53003e67bccaf54cc4b369
gomathid <30399036+gomathid@users.noreply.github.com>
2017-08-30 12:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title:'Article One | Gomathi',
+    heading:'Article One',
+    date:'Sep 5, 2016',
+    content:`<p>
+This is content of my first article...This is content of my first article...This is content of my first article...This is content of my first article...This is content of my first article...
+</p>
+<p>
+This is content of my first article...This is content of my first article...This is content of my first article...This is content of my first article...This is content of my first article...
+</p>
+<p>
+This is content of my first article...This is content of my first article...This is content of my first article...This is content of my first article...This is content of my first article...
+</p>`
+};
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
